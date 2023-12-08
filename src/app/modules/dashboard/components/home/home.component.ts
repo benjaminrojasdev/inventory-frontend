e2efa9a3c4e1ee08232ae16cf210c53b0e4e23ec
unknown <benjaminrojas824@gmail.com>
2023-12-08 19:56:32 -0300

    Proyecto Inventario Frontend

---
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DashboardModule } from '../../dashboard.module';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/modules/dashboard/pages/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DashboardModule } from '../dashboard.module';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
